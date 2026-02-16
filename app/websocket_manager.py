0e4add00db140d0b86993fac5c42d50447f4433f
Nahid Hasan <nahidhasan9613@gmail.com>
2026-02-16 16:51:46 +0600

    Implement WebSocketManager for handling client connections and messaging

---
+++ app/websocket_manager.py
@@ -0,0 +1,20 @@
+from fastapi import WebSocket
+from typing import Dict, List
+
+class WebSocketManager:
+    def __init__(self):
+        self.active_connections: Dict[str, WebSocket] = {}
+
+    async def connect(self, client_id: str, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections[client_id] = websocket
+
+    def disconnect(self, client_id: str):
+        if client_id in self.active_connections:
+            del self.active_connections[client_id]
+
+    async def send_personal_message(self, message: str, client_id: str):
+        if client_id in self.active_connections:
+            await self.active_connections[client_id].send_text(message)
+
+manager = WebSocketManager()
